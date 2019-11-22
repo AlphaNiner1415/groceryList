3f4e79ce0901f13037d154ff51ec84cdde0712e3
AlphaNiner1415 <manchesternd@gmail.com>
2019-11-22 16:55:41 +0700

    Pop over modifer

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  items =[0,1,2,3,4,5,6];
   constructor() {}
   squareclick(){
     console.log("Square number 2 was clicked!!!");
+  }
+  card2Clicked(){
+    console.log("Card 2 was clicked");
   }
 
 }
